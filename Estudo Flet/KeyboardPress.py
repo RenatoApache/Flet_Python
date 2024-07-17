318ae51c59b76d1afff68bfe0bd802044cbe8cde
RenatoApache <renatodtsato@gmail.com>
2024-07-17 14:43:06 -0300

    2Atualizando17.07

---
+++ Estudo Flet/KeyboardPress.py
@@ -0,0 +1,14 @@
+import flet as ft
+
+def app(page: ft.Page):
+
+    def tecla(e: ft.KeyboardEvent):
+        page.add(
+            ft.Text(f"Tecla pressionada: {e.key}, Shift: {e.shift}, Control: {e.ctrl}, Alt: {e.alt}, Meta:{e.meta}")
+        )        
+    
+    page.on_keyboard_event = tecla
+    page.add(
+        ft.Text("Pressione qualquer tecla ou uma combinação de teclas(CTR, SHIFT, ALT, META)...")
+    )
+ft.app(target=app)
